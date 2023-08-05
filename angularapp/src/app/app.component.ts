a4b5487fa9ba3042ccc5c3ea5cb9327d499e6e59
Iamneo <techadmin@iamneo.ai>
2023-08-05 17:00:06 +0000

    Backup Commit

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -1,5 +1,17 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'starting-angular';
+//   name = '';
+// }
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { ConvertPipe } from './convert.pipe';
 
 
 @Component({
--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,18 +1,35 @@
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { FormsModule } from '@angular/forms'; '@angular/forms'
+// import { AppComponent } from './app.component';
+
+// @NgModule({
+//   declarations: [
+//   ],
+//   imports: [
+//     BrowserModule,
+//     FormsModule
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-
+import { ConvertPipe } from './convert.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ConvertPipe
+    ConvertPipe,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-        ReactiveFormsModule
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
